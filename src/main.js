ea21fa66e8c087d0823bd6ad8624f2c8574be533
rulyox <email@rulyox.com>
2020-05-06 19:54:38 +0900

    Added theme

--- src/main.js
+++ src/main.js
@@ -1,21 +1,17 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import './theme/index.css';
 import App from './App';
 import router from './router';
 import store from './store';
 import request from './request';
 
 Vue.use(ElementUI);
-
 Vue.prototype.$request = request;
-
 Vue.config.productionTip = false;
 
 new Vue({
-
     render: h => h(App),
     store,
     router
-
 }).$mount('#app');
