43ae65b8c213156e8ddd41ad1b64aa2491d58f1d
rulyox <email@rulyox.com>
2020-07-14 15:40:32 +0900

    Moved common modal styles

--- src/components/modal/WriteModal.vue
+++ src/components/modal/WriteModal.vue
@@ -159,46 +159,4 @@
         flex-direction: column;
         align-items: center;
     }
-
-    /* Below are default settings */
-
-    .modal-mask {
-        position: fixed;
-        z-index: 9998;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, .5);
-        display: table;
-        transition: opacity .3s ease;
-    }
-
-    .modal-wrapper {
-        display: table-cell;
-        vertical-align: middle;
-    }
-
-    /*
-     * The following styles are auto-applied to elements with
-     * transition="modal" when their visibility is toggled
-     * by Vue.js.
-     *
-     * You can easily play with the modal transition by editing
-     * these styles.
-     */
-
-    .modal-enter {
-        opacity: 0;
-    }
-
-    .modal-leave-active {
-        opacity: 0;
-    }
-
-    .modal-enter .modal-container,
-    .modal-leave-active .modal-container {
-        -webkit-transform: scale(1.1);
-        transform: scale(1.1);
-    }
 </style>
