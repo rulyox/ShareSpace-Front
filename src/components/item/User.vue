27bec90e6984979b85b08961ccc8da010be158c8
rulyox <email@rulyox.com>
2020-07-14 13:28:22 +0900

    Added outer shadow to user component

--- src/components/item/User.vue
+++ src/components/item/User.vue
@@ -82,13 +82,23 @@
 
 <style scoped>
     .user-container {
+        width: 500px;
         padding: 15px;
+        margin-bottom: 15px;
 
         cursor: pointer;
 
         display: flex;
         flex-direction: row;
         align-items: center;
+
+        border-radius: 10px;
+        box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+        transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+    }
+
+    .user-container:hover {
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
     }
 
     .user-image {
